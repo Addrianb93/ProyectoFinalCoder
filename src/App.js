b078fb1977db80cb6f992d5fe4f1398b87a65344
AdrianBerardo <adrianberardomatias@gmail.com>
2022-10-15 15:39:51 -0300

    Creacion del proyecto a entregar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+
+    <Header
+      NombrePagina="Pagina Web"
+      A
+    />
+    <Footer/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(Titulo) {
+  return (
+      <header className='fondo'>
+        <p>Encabezado</p>
+      </header>
+  );
+}
+
+export default Header;
